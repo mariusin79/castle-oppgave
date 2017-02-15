7eddd4db888702730b58522e7acc0213ec5c86b4
Marius Ingjer <mariusin79@gmail.com>
2017-02-15 08:25:31 +0100

    explicit

---
+++ Utviklerforum/Registrering.cs
@@ -0,0 +1,25 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using NUnit.Framework;
+
+namespace Utviklerforum
+{
+	public class Registrering
+	{
+		public void Explicit_Registration()
+		{
+			var container = new WindsorContainer();
+
+			container.Register(Component.For<SomeClass>());
+
+			var someClass = container.Resolve<SomeClass>();
+
+			Assert.That(someClass, Is.Not.Null);
+		}
+
+	}
+
+	public class SomeClass
+	{
+	}
+}
