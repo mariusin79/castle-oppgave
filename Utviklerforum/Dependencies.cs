6ebc94826e5f49ff74d916b8abbe3e47ba311f82
Marius Ingjer <mariusin79@gmail.com>
2017-02-15 15:52:20 +0100

    f

---
+++ Utviklerforum/Dependencies.cs
@@ -0,0 +1,38 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using NUnit.Framework;
+
+namespace Utviklerforum
+{
+	public class Dependencies
+	{
+
+		public void Mandatory_Dependencies()
+		{
+			var container = new WindsorContainer();
+
+			container.Register(Component.For<SomeDependency>());
+			container.Register(Component.For<SomeClass>());
+
+			var instance = container.Resolve<SomeClass>();
+
+			Assert.That(instance.Dependency, Is.Not.Null);
+		}
+
+
+		public class SomeClass
+		{
+			public SomeDependency Dependency { get; }
+
+			public SomeClass(SomeDependency dependency)
+			{
+				Dependency = dependency;
+			}
+		}
+
+		public class SomeDependency
+		{
+
+		}
+	}
+}
