5ccde3968950c0dc868dcd62ab1f3fed109f6a80
Marius Ingjer <mariusin79@gmail.com>
2017-02-16 11:08:11 +0100

    installers

---
+++ Utviklerforum/Installers.cs
@@ -0,0 +1,49 @@
+﻿using Castle.DynamicProxy;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using NUnit.Framework;
+
+namespace Utviklerforum
+{
+	public class Installers
+	{
+
+		public void Installer_Installs_Components()
+		{
+			var container = new WindsorContainer();
+
+			container.Install(FromAssembly.This());
+
+			var someInterceptor = container.Resolve<SomeInterceptor>();
+
+			Assert.That(someInterceptor, Is.Not.Null);
+		}
+
+
+		public class MyInstaller: IWindsorInstaller
+		{
+			public void Install(IWindsorContainer container, IConfigurationStore store)
+			{
+				container.Register(Classes.FromThisAssembly().BasedOn<IInterceptor>().WithServiceSelf());
+			}
+		}
+
+		public class SomeInterceptor: IInterceptor
+		{
+			public void Intercept(IInvocation invocation)
+			{
+				throw new System.NotImplementedException();
+			}
+		}
+
+		public class AnotherInterceptor : IInterceptor
+		{
+			public void Intercept(IInvocation invocation)
+			{
+				throw new System.NotImplementedException();
+			}
+		}
+	}
+}
