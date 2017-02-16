62efccd779ae8566633737b94f1efdd0b8d88bd9
Marius Ingjer <mariusin79@gmail.com>
2017-02-16 11:05:01 +0100

    konvensjonsbasert registrering

---
+++ Utviklerforum/KonvensjonsbasertRegistrering.cs
@@ -0,0 +1,34 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using NUnit.Framework;
+
+namespace Utviklerforum
+{
+	public class KonvensjonsbasertRegistrering
+	{
+		public void Register_By_Naming_Convention()
+		{
+			var container = new WindsorContainer();
+
+			container.Register(Classes.FromThisAssembly().Where(type => type.Name.EndsWith("Repo")));
+
+			var someRepo = container.Resolve<SomeRepo>();
+
+			Assert.That(someRepo, Is.Not.Null);
+		}
+
+
+
+
+		public class SomeRepo
+		{
+			
+		}
+
+		public class AnotherRepo
+		{
+		
+		}
+
+	}
+}
